5f3356f6eb377edd1ec87af134b6c62f31c2e4a6
h0lms <24info1mail@gmail.com>
2020-06-28 17:39:40 +0300

    2menu

--- js/menu.js
+++ js/menu.js
@@ -5,14 +5,35 @@ hamburger.addEventListener("click", e => {
   e.preventDefault();
   menu.classList.add("menu--mobile");
 
-  const closeElement = document.createElement("div");
-  closeElement.classList.add("close");
-  closeElement.innerHTML = "x";
+  hamburger.classList.add("hamburger--close");
 
-  closeElement.addEventListener("click", e => {
+  hamburger.style.position = "fixed";
+  hamburger.style.zIndex = "1000";
+
+  hamburger.addEventListener("click", e => {
     menu.classList.remove("menu--mobile");
-    menu.removeChild(closeElement);
+    hamburger.classList.remove("hamburger--close");
+    hamburger.removeAttribute("style");
   })
 
-  menu.appendChild(closeElement);
 })
+
+
+// const menu = document.querySelector(".menu");
+// const hamburger = document.querySelector(".hamburger");
+
+// hamburger.addEventListener("click", e => {
+//   e.preventDefault();
+//   menu.classList.add("menu--mobile");
+
+//   const closeElement = document.createElement("div");
+//   closeElement.classList.add("close");
+//   closeElement.innerHTML = "x";
+
+//   closeElement.addEventListener("click", e => {
+//     menu.classList.remove("menu--mobile");
+//     menu.removeChild(closeElement);
+//   })
+
+//   menu.appendChild(closeElement);
+// })
