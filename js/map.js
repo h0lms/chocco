6d1d7b2ddea41c5b8f2c7c2f46eb6593e3f97793
h0lms <24info1mail@gmail.com>
2020-07-09 00:49:12 +0300

    yandex map

---
+++ js/map.js
@@ -0,0 +1,34 @@
+let myMap;
+
+const init = () => {
+  myMap = new ymaps.Map("map", {
+    center: [55.753969, 37.606673],
+    zoom: 14,
+    controls: []
+  });
+
+  const coords =  [
+    [55.763183, 37.582547],
+    [55.741035, 37.573998],
+    [55.746456, 37.608084],
+    [55.761756, 37.618330]
+  ];
+
+  const myCollection = new ymaps.GeoObjectCollection({}, {
+    draggable: false,
+    iconLayout: 'default#image',
+    iconImageHref: "./img/icons/marker.svg",
+    iconImageSize: [46, 57],
+    iconImageOffset: [-35, -52]
+  });
+
+  coords.forEach(coord => {
+    myCollection.add(new ymaps.Placemark(coord));
+  });
+
+  myMap.geoObjects.add(myCollection);
+
+  myMap.behaviors.disable('scrollZoom');
+}
+
+ymaps.ready(init); 
